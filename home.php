4fa18bd2cd1a4cfdaa3a5ae75d3cc25901f0b65d
Senthur-Ravi <69335391+Senthur-Ravi@users.noreply.github.com>
2021-03-23 20:10:17 +0530

    Add files via upload

---
+++ home.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<link rel="stylesheet" type="text/css" href="css/home.css"/>
+<title>Apple College of Technology</title>
+</head>
+
+<body>
+	<div class="panel panel-default">
+  		<div class="panel-heading"><h1><font color = "grey"><b><strong> Apple College</strong> </b></font> </h1></div>
+                <div class="panel-body"><br><br>
+        <div>
+            <img src="images/rtupic.jpg" width="855" style="text-align: center;" height="308" onmousedown="return false;"  />
+        </div><br><br>
+        <div class="middle_text">
+            <p><strong>Apple College of Techonlogy, Affiliated to Google, has a residence in Sathyamangalam, Erode. The college name is subjected to Copyright, 2020 Inc. 	</strong></p>
+        </div>
+                
+        <div class="text_para_home">
+            <p>Apple College of Technology is a great gesture which is found by Mr. Senthur.R and his colleagues. This college has amazing features of technology that every engineering student deserves it. The name 'Apple' was suggested by his one of his Colleagues called as Pavithran Astrologer.  
+As of April 2020, RTU affiliates 202 colleges. B.Tech degrees are offered at Apple Institute of Technology and has B.E in every engineering aspects. In this college, stricly there are no semester exams, the students skills are evaluated using their practical knowledge. All types electroninc applications are allowed in the campus. Students with high skills and knowledge would be gifted a Updated Iphones every year. Free wifi at the rate 1GBPS is provided for a hub. Students can willingly attend the class or else they can do their project without any one acknowledgement. Strictly attendance is not necessary. College fees are not so high, It varies from 25000 to 30000 for a year.</p>         
+        </div>   
+                </div>
+        </div>
+    
+    
+</body>
+</html>
